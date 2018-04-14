51083e8464ba06d042bc87d296432d1219774cb8
Sensoume <theile.marco@t-online.de>
2018-04-14 12:06:42 +0200

    updated splash function and updated cocktaillist"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { EinstellungenPage } from "../pages/einstellungen/einstellungen";
 import { CocktailsPage } from "../pages/cocktails/cocktails";
 import { ConnectPage } from "../pages/connect/connect";
--- src/pages/cocktails/cocktails.ts
+++ src/pages/cocktails/cocktails.ts
@@ -17,11 +17,18 @@ export class CocktailsPage {
 
   toastOptions:ToastOptions;
 
+  cards: Array<{url: string, title: string, subtitle:string}>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController) {
     this.toastOptions = {
       message: "Flasche gewählt",
       duration: 3000
     }
+
+    this.cards = [
+      { url: 'assets/imgs/cocktail-sexonthebeach.jpg', title: 'Sex on the Beach', subtitle: 'So verführerisch wie sein Name: Sex on the Beach. Die fruchtige Versuchung im Longdrink-Format verwandelt Pfirsichlikör, Orangensaft und Cranberry in einen Wodka-Cocktail mit Highball-Anleihen. Übrigens – ohne Wodka wird’s zum Safer Sex on the Beach!' },
+      { url: 'assets/imgs/pina-colada-cocktail.jpg', title: 'Pina Colada', subtitle: 'Karibik pur! Mit einer Pina Colada kommt sofort Urlaubsstimmung auf. Der tropisch-fruchtige Sommer-Cocktail verwandelt Ananas, braunen und weißen Rum, Kokos und Sahne in eine wunderbar cremige Mischung, die auch in unseren Breiten Tiki-Kultur aufleben lässt.\n'}
+    ];
   }
 
   ButtonPressed() {
